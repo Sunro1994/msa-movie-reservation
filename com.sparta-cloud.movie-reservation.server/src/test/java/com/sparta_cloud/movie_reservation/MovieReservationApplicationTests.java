408b47116855aa474aec425ca0a418a27c43a34a
Sunro1994 <gwangjulsr@gmail.com>
2025-02-06 11:29:59 +0900

    Eureka Server initial Commit

---
+++ com.sparta-cloud.movie-reservation.server/src/test/java/com/sparta_cloud/movie_reservation/MovieReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta_cloud.movie_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
