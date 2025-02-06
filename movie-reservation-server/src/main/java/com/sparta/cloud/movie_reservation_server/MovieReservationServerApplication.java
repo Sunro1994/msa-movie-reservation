9e4f252b7b92f8b89aa835c397d99b1e8fd6007c
Sunro1994 <gwangjulsr@gmail.com>
2025-02-06 11:51:39 +0900

    Eureka Server initial commit

---
+++ movie-reservation-server/src/main/java/com/sparta/cloud/movie_reservation_server/MovieReservationServerApplication.java
@@ -0,0 +1,14 @@
+package com.sparta.cloud.movie_reservation_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MovieReservationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservationServerApplication.class, args);
+	}
+
+}
