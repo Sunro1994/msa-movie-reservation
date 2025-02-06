253c87026ce836555290966f4f5ef936aec0b159
Sunro1994 <gwangjulsr@gmail.com>
2025-02-06 12:25:09 +0900

    User Server Initial Commit

---
+++ movie-reservation-user/src/main/java/com/sparta/cloud/movie_reservation_user/MovieReservationUserApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cloud.movie_reservation_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservationUserApplication.class, args);
+	}
+
+}
