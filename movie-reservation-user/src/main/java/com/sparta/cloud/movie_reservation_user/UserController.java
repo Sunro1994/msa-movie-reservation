d130dc9abbd9bb03c57f58d4a120718f7d2670bf
Sunro1994 <gwangjulsr@gmail.com>
2025-02-06 15:38:35 +0900

    requestMapping url convention 위반 내용 수정

--- movie-reservation-user/src/main/java/com/sparta/cloud/movie_reservation_user/UserController.java
+++ movie-reservation-user/src/main/java/com/sparta/cloud/movie_reservation_user/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
