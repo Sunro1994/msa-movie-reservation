a0ddf04ac3ffb0a29a9a5f61240eb1de9fe185f3
Sunro1994 <gwangjulsr@gmail.com>
2025-02-06 15:35:04 +0900

    gateway server initial commit

---
+++ movie-reservation-gateway/src/main/java/com/sparta/cloud/movie_reservation_gateway/MovieReservationGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cloud.movie_reservation_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservationGatewayApplication.class, args);
+	}
+
+}
