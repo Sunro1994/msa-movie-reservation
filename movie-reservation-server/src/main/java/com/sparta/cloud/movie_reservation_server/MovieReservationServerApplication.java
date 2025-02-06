6cc057e396532b7e5a8c86f6fd7c155382b59879
Sunro1994 <gwangjulsr@gmail.com>
2025-02-06 12:22:26 +0900

    Server 통신 설정

--- movie-reservation-server/src/main/java/com/sparta/cloud/movie_reservation_server/MovieReservationServerApplication.java
+++ movie-reservation-server/src/main/java/com/sparta/cloud/movie_reservation_server/MovieReservationServerApplication.java
@@ -2,9 +2,10 @@ package com.sparta.cloud.movie_reservation_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
+@EnableEurekaServer
 public class MovieReservationServerApplication {
 
 	public static void main(String[] args) {
